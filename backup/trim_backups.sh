961d7994f9ede4a529f5e052f5ec2441b5b97681
jas0ndiamond <jason.a.diamond@gmail.com>
2025-03-29 22:18:01 -0700

    fix backup counter. update naming

--- backup/trim_backups.sh
+++ backup/trim_backups.sh
@@ -27,9 +27,9 @@ fi
 
 
 # trim backups if we're over our maximum
-BACKUP_COUNT=$(/bin/ls -l "$BACKUP_DIR" | grep -c "^total")
+BACKUP_COUNT=$(/bin/ls -l "$BACKUP_DIR" | grep -cv "^total")
 if [ "$BACKUP_COUNT" -gt $MIN_KEEP ]; then
-        /bin/ls -l "$BACKUP_DIR" | grep "backup_.*\.xml$" | head -$MAX_DEL | awk -v mydir="$BACKUP_DIR" '{print mydir"/"$NF}' | xargs rm -v
+        /bin/ls -l "$BACKUP_DIR" | grep "nssk_data_dump_.*\.xml$" | head -$MAX_DEL | awk -v mydir="$BACKUP_DIR" '{print mydir"/"$NF}' | xargs rm -v
 else
         echo "Skipping backup trim for backup count $BACKUP_COUNT in directory $BACKUP_DIR"
 fi
