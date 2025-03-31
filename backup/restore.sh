8f1df5cec4ef43a6fff739806d317f44497b226c
jas0ndiamond <jason.a.diamond@gmail.com>
2025-03-31 11:44:46 -0700

    add restore script to wrap mysql restore

---
+++ backup/restore.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# typically this is done manually but this script attempts to streamline stuff
+# will attempt to create databases if they don't exist
+
+CONF_FILE=$1
+DB_DUMP_FILE=$2
+
+##################################
+# check if we have jq
+which jq > /dev/null
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+  echo "Could not find jq on PATH. Please install jq. Exiting..."
+  exit 1
+fi
+
+##################################
+# check if we have mysqldump. backups can be run remotely so it's not ridiculous, and it's quick to check.
+
+which mysql > /dev/null
+RESULT=$?
+
+if [ $RESULT -ne 0 ]; then
+  echo "Could not find mysql on PATH. Please install mysql. Exiting..."
+  exit 1
+fi
+
+##################################
+# read credentials for our restore user
+
+if [ -z "$CONF_FILE" ]; then
+  echo "Need config file"
+  exit 1
+fi
+
+if [ ! -f "$CONF_FILE" ]; then
+  echo "Config file does not exist"
+  exit 1
+fi
+
+# hardcode for now
+USER="nssk_admin"
+
+# read from config
+HOST="$(jq -r '.network.listen_ip' < "$CONF_FILE")"
+PORT="$(jq -r '.network.listen_port' < "$CONF_FILE")"
+PASS="$(jq -r '.users.nssk_admin' < "$CONF_FILE")"
+
+if [[ -z $HOST ]]; then
+  echo "Could not read host"
+  exit 1
+fi
+
+if [[ -z $PORT ]]; then
+  echo "Could not read port"
+  exit 1
+fi
+
+if [[ -z $USER ]]; then
+  echo "Could not read user"
+  exit 1
+fi
+
+if [[ -z $PASS ]]; then
+  echo "Could not read password"
+  exit 1
+fi
+
+##################################
+# check we have a valid dump file
+
+if [ -z "$DB_DUMP_FILE" ]; then
+  echo "Need database dump file"
+  exit 1
+fi
+
+if [ ! -f "$DB_DUMP_FILE" ]; then
+  echo "Database dump file does not exist"
+  exit 1
+fi
+
+# ends in .sql
+#if [[ "$DB_DUMP_FILE" =~ \.(?i)sql$ ]]; then
+if [[ "$DB_DUMP_FILE" =~ \.[sS][qQ][lL]$ ]]; then
+  echo "Database dump file extension check passed"
+else
+  echo "Database dump file has unexpected extension. Extension must end with '.sql'"
+  exit 1
+fi
+
+# hmmm seeing some directive set at top of file when the backup script is run with cron
+# first line of file matches "MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)"
+#if read -r line < "$DB_DUMP_FILE" && [[ "$line" == "-- MariaDB dump "* ]]; then
+#    echo "Database dump file preamble check passed"
+#else
+#    echo "Database dump file preamble check failed. Dump file must begin with '-- MariaDB dump '"
+#    exit 1
+#fi
+
+
+
+# would it be easy to check that all tables are empty? maybe but just throw this in the warning
+
+
+####################
+# final prompt before running restore
+
+# WARNING prompt to confirm a fresh database instance is at host/port, and is ready
+# warn about restoring to a hot db
+
+echo "============================================================"
+echo "WARNING: You are attempting a database restore."
+echo -e "\tDatabase Dump File: $DB_DUMP_FILE"
+echo -e "\tDatabase: $HOST:$PORT"
+echo -e "\tUser: $USER"
+echo "This is often a destructive operation, and may result in data loss."
+echo "Ensure that the NSSK database instance is empty and ready for restore."
+echo "============================================================"
+read -r -p "Proceed? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || { echo "Did not get confirmation. Bailing..."; exit 1; }
+
+CMD="mysql -h $HOST -P $PORT -u $USER --password=\"$PASS\" -f < $DB_DUMP_FILE"
+
+#echo "$CMD"
+
+eval "$CMD"
